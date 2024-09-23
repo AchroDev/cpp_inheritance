f8b91edecf513be1a2a2731cc969d3eacd886d19
AchroDev <achrodev@proton.me>
2024-09-23 18:10:12 -0500

    Added example Entity and Player classes with code and comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 
+// Example Entity Class
+class Entity
+{
+public:
+    float X, Y;
+
+    // Example Move function taking two parameters 'xa' AND 'ya'
+    void Move(float xa, float ya)
+    {
+        X += xa;
+        Y += ya;
+    }
+};
+
+// Example Player class that has two types 'Player' AND 'Entity'
+class Player : public Entity
+{
+public:
+    const char *Name;
+    float X, Y;
+
+    void Move(float xa, float ya)
+    {
+        X += xa;
+        Y += ya;
+    }
+
+    void PrintName()
+    {
+        std::cout << Name << std::endl;
+    }
+};
+
 int main()
 {
 
